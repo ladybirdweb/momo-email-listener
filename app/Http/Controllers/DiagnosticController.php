6f19362710e921fc1b9f7ff8c38cd0de9ff919de
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-05-11 18:00:13 +0530

    Update v1.0.1

--- app/Http/Controllers/DiagnosticController.php
+++ app/Http/Controllers/DiagnosticController.php
@@ -25,8 +25,8 @@ class DiagnosticController extends Controller {
         try {
             $emails = $email->all();
             return view('diagnostic.index', compact('emails'));
-        } catch (Exception $ex) {
-            return redirect()->back()->with('fail', $e);
+        } catch (Exception $e) {
+            return redirect()->back()->with('fails', $e);
         }
     }
 
@@ -36,41 +36,55 @@ class DiagnosticController extends Controller {
      */
     public function postDiag(DiagnosRequest $request) {
         try {
-            $mail = new \PHPMailer;
+            $email_details = Emails::where('id', '=', $request->from)->first();
+            if ($email_details->sending_protocol == 'mail') {
+                $mail = new PHPMailer(); // defaults to using php "mail()"
+                $mail->IsSendmail(); // telling the class to use SendMail transport
+//                $body = file_get_contents('contents.html');
+//                $body = eregi_replace("[\]", '', $request->message);
+                $mail->AddReplyTo($request->from, "");
+                $mail->SetFrom($request->from, "");
+                $mail->AddReplyTo($request->from, "");
+                $address = $request->to;
+                $mail->AddAddress($address, "");
+                $mail->Subject = $request->subject;
+                $mail->MsgHTML($request->message);
+                if (!$mail->Send()) {
+                    $return = "Mailer Error: " . $mail->ErrorInfo;
+                } else {
+                    $return = "Message sent from Php-Mail";
+                }
+            } elseif ($email_details->sending_protocol == 'smtp') {
+                $mail = new \PHPMailer;
+                //$mail->SMTPDebug = 3;                                     // Enable verbose debug output
+                $mail->isSMTP();                                            // Set mailer to use SMTP
+                $mail->Host = $email_details->sending_host;                 // Specify main and backup SMTP servers
+                $mail->SMTPAuth = true;                                     // Enable SMTP authentication
+                $mail->Username = $email_details->email_address;                 // SMTP username
+                $mail->Password = \Crypt::decrypt($email_details->password);                           // SMTP password
+                $mail->SMTPSecure = $email_details->sending_encryption;                            // Enable TLS encryption, `ssl` also accepted
+                $mail->Port = $email_details->sending_port;                                    // TCP port to connect to
 
-            //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+                $mail->setFrom($email_details->email_address, $email_details->email_name);
+                $mail->addAddress($email_details->email_address, $email_details->email_name);     // Add a recipient
+//                $mail->addCC('cc@example.com');
+//                $mail->addBCC('bcc@example.com');
+//                $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+                $mail->isHTML(true);                                  // Set email format to HTML
 
-            $mail->isSMTP();                                      // Set mailer to use SMTP
-            $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-            $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = 'user@example.com';                 // SMTP username
-            $mail->Password = 'secret';                           // SMTP password
-            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587;                                    // TCP port to connect to
-
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-            $mail->addAddress('ellen@example.com');               // Name is optional
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
-
-            $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-            $mail->isHTML(true);                                  // Set email format to HTML
-
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            if (!$mail->send()) {
-                echo 'Message could not be sent.';
-                echo 'Mailer Error: ' . $mail->ErrorInfo;
-            } else {
-                echo 'Message has been sent';
+                $mail->Subject = $request->subject;
+                $mail->Body = $request->message;
+//dd($mail);
+                if (!$mail->send()) {
+                    $return = 'Mailer Error: ' . $mail->ErrorInfo;
+                } else {
+                    $return = 'Message has been sent';
+                }
             }
-        } catch (Exception $ex) {
-
+            return redirect()->back()->with('success', $return);
+        } catch (Exception $e) {
+            return redirect()->back()->with('fails', $e->getMessage());
         }
     }
 
